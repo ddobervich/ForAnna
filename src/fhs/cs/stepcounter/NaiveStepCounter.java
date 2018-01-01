82c3019772831503e3a14bb1ba50beea5f5513ca
David Dobervich <david.dobervich@gmail.com>
2018-01-01 12:28:35 -0800

    add two method stubs to aide in vizualization

--- src/fhs/cs/stepcounter/NaiveStepCounter.java
+++ src/fhs/cs/stepcounter/NaiveStepCounter.java
@@ -5,16 +5,15 @@ import fhs.cs.stepcounter.interfaces.StepCounter;
 
 public class NaiveStepCounter implements StepCounter {
 
-
 	/***
 	 * Return the number of steps represented by the data in CSVData object.
 	 * 
 	 * @param data
 	 *            a CSVData object which is a wrapper for the raw sensor data.
-	 *            Extract the specific data you want using the
-	 *            .getDataForColumns method. You can specify column names to get
-	 *            a 2d array of the data.
-	 * @return the number of steps represented by the data. 
+	 *            Extract the specific data you want using the .getDataForColumns
+	 *            method. You can specify column names to get a 2d array of the
+	 *            data.
+	 * @return the number of steps represented by the data.
 	 */
 	@Override
 	public int countSteps(CSVData data) {
@@ -43,20 +42,41 @@ public class NaiveStepCounter implements StepCounter {
 
 	/***
 	 * Takes a 2d array with 3 columns representing the 3 axes of a sensor.
-	 * Calculates the magnitude of the vector represented by each row. Returns a
-	 * new array with the same number of rows where each element contains this
+	 * Calculates the magnitude of the vector represented by each row. Returns a new
+	 * array with the same number of rows where each element contains this
 	 * magnitude.
 	 * 
 	 * @param accelData
-	 *            2d array of 3 axis acceleration data. Column 0 is x, column 1
-	 *            is y, column 2 is z
+	 *            2d array of 3 axis acceleration data. Column 0 is x, column 1 is
+	 *            y, column 2 is z
 	 * 
-	 * @return an array with n rows and each element is the magnitude of the
-	 *         vector for the corresponding row in the sensorData array
+	 * @return an array with n rows and each element is the magnitude of the vector
+	 *         for the corresponding row in the sensorData array
 	 */
 	private static double[] calculateMagnitudesFor(double[][] accelData) {
 
 		return null;
+	}
+
+	/***
+	 * Return an array of values that can be used to graph. These could be values
+	 * for one sensor, or the result of calculations involving several sensors.
+	 * 
+	 * @return an array of sensor values for graphing
+	 */
+	public double[] getDataForGraphing() {
+		return null;
+	}
+
+	/***
+	 * Return an array giving the indexes in getDataForGraphing() where you
+	 * calculate steps occurring. This can be used to visualize/debug.
+	 * 
+	 * @return an array where the ith element contains the index corresponding to
+	 *         the ith step in getDataForGraphing()
+	 */
+	public int[] getStepIndexes() {
+
 	}
 
 	/***
